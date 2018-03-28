c3f38b335e2c14d9ce623321ad1df96c111ee965
morecodemore <ulyanov2022@gmail.com>
2018-03-28 22:25:58 +0300

    fx info-modal

--- src/lang/ru/info.js
+++ src/lang/ru/info.js
@@ -1,4 +1,8 @@
 export default {
   title: 'Описание процесса разработки',
-  text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at consectetur delectus deleniti deserunt dicta ducimus ea esse est et hic illo ipsam iure laudantium libero minima minus molestiae molestias nam nesciunt nihil nobis, nulla omnis perspiciatis quam quas quibusdam quod reiciendis repellat totam. A commodi consequuntur delectus deleniti dolorem doloribus, ducimus eaque est ex fugit illo ipsa labore laboriosam magnam magni, maiores nihil nobis nulla officia, pariatur perferendis perspiciatis praesentium quaerat quis repellendus reprehenderit saepe sit soluta tempore ut vel veniam voluptatem voluptatibus? Asperiores, iste maxime nobis obcaecati officiis quam vero vitae. Accusamus minima modi, optio quos tempora totam?'
+  text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at consectetur delectus deleniti deserunt dicta ducimus ea esse est et hic illo ipsam iure laudantium libero minima minus molestiae molestias nam nesciunt nihil nobis, nulla omnis perspiciatis quam quas quibusdam quod reiciendis repellat totam. A commodi consequuntur delectus deleniti dolorem doloribus, ducimus eaque est ex fugit illo ipsa labore laboriosam magnam magni, maiores nihil nobis nulla officia, pariatur perferendis perspiciatis praesentium quaerat quis repellendus reprehenderit saepe sit soluta tempore ut vel veniam voluptatem voluptatibus? Asperiores, iste maxime nobis obcaecati officiis quam vero vitae. Accusamus minima modi, optio quos tempora totam?',
+  projecting:'Проектирование',
+  design:'Дизайн',
+  page_making:'Верстка',
+  frontend:'Фронтенд'
 }
